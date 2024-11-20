370d6d4886daf9cd500e5aba0a1fc4796d767415
tuananhysl <anhx2004@gmail.com>
2024-11-21 00:43:28 +0700

    ok

--- harmic/obj/Debug/net8.0/harmic.AssemblyInfo.cs
+++ harmic/obj/Debug/net8.0/harmic.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("harmic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+902bd9044e9c1389576db481797fe7b4538d0f51")]
 [assembly: System.Reflection.AssemblyProductAttribute("harmic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("harmic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
